6308aa67a287d4c0ae64b04e3735fac218ece502
akikr <ankit.kumar962@gmail.com>
2022-05-14 22:38:22 +0530

    Modified: The CassandraDataLoaderApplication class to run the data loader services from the main function

--- src/main/java/io/akikr/betterreadsdataloader/CassandraDataLoaderApplication.java
+++ src/main/java/io/akikr/betterreadsdataloader/CassandraDataLoaderApplication.java
@@ -1,7 +1,10 @@
 package io.akikr.betterreadsdataloader;
 
+import io.akikr.betterreadsdataloader.service.CassandraDataLoaderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @apiNote Main class for CassandraDataLoaderApplication
@@ -9,10 +12,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0
  */
 
+@Slf4j
 @SpringBootApplication
 public class CassandraDataLoaderApplication
 {
-	public static void main(String[] args) {
-		SpringApplication.run(CassandraDataLoaderApplication.class, args);
+	public static void main(String[] args)
+	{
+		ConfigurableApplicationContext context = SpringApplication.run(CassandraDataLoaderApplication.class, args);
+
+		// Run the CassandraDataLoaderService
+		CassandraDataLoaderService cassandraDataLoaderService = context.getBean(CassandraDataLoaderService.class);
+		cassandraDataLoaderService.run();
+
+		// Exit application after all data loader services are completed
+		System.exit(SpringApplication.exit(context, () -> {
+			log.info("Exiting CassandraDataLoaderApplication !!");
+			return 0;
+		}));
 	}
 }
