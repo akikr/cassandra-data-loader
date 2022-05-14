a2258525c857d2eda2b286a55d87362abeb7e45e
akikr <ankit.kumar962@gmail.com>
2022-05-14 22:16:51 +0530

    Initial Commit: Cassandra data loader

---
+++ src/main/java/io/akikr/betterreadsdataloader/CassandraDataLoaderApplication.java
@@ -0,0 +1,18 @@
+package io.akikr.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @apiNote Main class for CassandraDataLoaderApplication
+ * @author ankit
+ * @since 1.0
+ */
+
+@SpringBootApplication
+public class CassandraDataLoaderApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraDataLoaderApplication.class, args);
+	}
+}
---
+++ src/test/java/io/akikr/betterreadsdataloader/CassandraDataLoaderApplicationTests.java
@@ -0,0 +1,9 @@
+package io.akikr.betterreadsdataloader;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraDataLoaderApplicationTests
+{
+
+}
