a7bc6098316b455a73c069a7f5349c77ba532f80
akikr <ankit.kumar962@gmail.com>
2022-05-14 22:28:03 +0530

    Added: The author & book entity and their respective repositories

---
+++ src/main/java/io/akikr/betterreadsdataloader/db/entity/Book.java
@@ -0,0 +1,52 @@
+package io.akikr.betterreadsdataloader.db.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @implNote: A Book entity to persist the book object in database
+ * @author ankit
+ * @since 1.0
+ */
+
+@Data
+@Table(value = "book_by_id")
+public class Book
+{
+    @Id
+    @PrimaryKeyColumn(name = "book_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private String id;
+
+    @Column("book_name")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String name;
+
+    @Column("book_description")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    private String description;
+
+    @Column("published_date")
+    @CassandraType(type = CassandraType.Name.DATE)
+    private LocalDate publishedDate;
+
+    @Column("cover_ids")
+    @CassandraType(type = CassandraType.Name.LIST, typeArguments = CassandraType.Name.TEXT)
+    private List<String> coverIds;
+
+    @Column("author_names")
+    @CassandraType(type = CassandraType.Name.LIST, typeArguments = CassandraType.Name.TEXT)
+    private List<String> authorNames;
+
+    @Column("author_ids")
+    @CassandraType(type = CassandraType.Name.LIST, typeArguments = CassandraType.Name.TEXT)
+    private List<String> authorIds;
+
+}
